c143b094b745dbb2fede6f1e7ea44e79567836e5
carlos.chavira333 <carlos.chavira333@gmail.com>
2022-05-24 05:41:09 -0500

    pushing arrays ex process (create person objects)

--- src/ArrayExercises.java
+++ src/ArrayExercises.java
@@ -1,21 +1,42 @@
 import java.util.Arrays;
-
 //Create a class inside of src named ArraysExercises.
 public class ArrayExercises {
 //Create a main method for this class to do your work.
-public static void main(String[] args) {
+    public static void main(String[] args) {
 //    What happens when you run the following code?
-    int[] numbers = {1, 2, 3, 4, 5};
+//        int[] numbers = {1, 2, 3, 4, 5};
 //    System.out.println(numbers);
+        // It printed a memory location.
 
-    // add Arrays.toString
-    System.out.println(Arrays.toString(numbers));
-
-// It printed a memory location.
+        // add Arrays.toString
+        // System.out.println(Arrays.toString(numbers));
 
 // Why is Arrays.toString necessary? Answer: It Returns a string representation of the array.
 // Answer continued: adding Arrays.toString gives us [1, 2, 3, 4, 5]
 
-}
+// Person exercise main section here
+// create an array that holds 3 Person objects. Assign a new instance of the Person class to each element. Iterate through the array and print out the name of each person in the array.
 
-}
+        // create an array that holds 3 Person objects.
+        Person arrPersonObjs[] = new Person[3];
+        // Assign a new instance of the Person class to each element.
+        arrPersonObjs[0] = new Person("Codey the duck");
+        arrPersonObjs[1] = new Person("Thundercat");
+        arrPersonObjs[2] = new Person("Kermit the Frog");
+        System.out.println(Arrays.toString(arrPersonObjs));
+        // Iterate through the array and print out the name of each person in the array.
+        for(Person personObj : arrPersonObjs)
+            System.out.println(personObj.getName());
+
+// Create a static method named addPerson. It should accept an array of Person objects, as well as a single person object to add to the passed array. It should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array.
+
+
+        }
+
+
+
+
+    }
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        //declare and initialize a string array
+        String[] numArray = {"one","two", "three", "four", "five"};
+        System.out.println("String Array elements displayed using for loop:");
+        // for loop to iterate over the string array
+        for(int i=0; i<numArray.length;i++)
+            System.out.print(numArray[i] + " ");
+
+        System.out.println("\n new line");
+
+        System.out.println("String Array elements displayed using enhanced for loop:");
+        //enhanced for loop to iterate over the string array
+        for(String val:numArray)
+            System.out.print(val + " ");
+
+        //declare and initialize a string array
+        long[] stocks = {10L, 20L, 30L, 40L, 50L, 600L, 70L};
+        for(int i=0; i<stocks.length;i++)
+            System.out.print(stocks[i] + " ");
+
+        // Let's create a method that returns the sum of all integers in an int array (nums)
+
+
+    }
+}
