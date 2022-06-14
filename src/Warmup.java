425208d013a247cffb5d340d69b3e9be6245c5dc
carlos.chavira333 <carlos.chavira333@gmail.com>
2022-06-14 08:50:34 -0500

    self warm up: reverse a string

--- src/Warmup.java
+++ src/Warmup.java
@@ -1,7 +1,7 @@
 public class Warmup {
     public static void main(String[] args) {
-//        fizzBuzz(1);
         System.out.println(countWords("abc"));// returns 3
+        System.out.println(reverse("Today")); //yadseut si yadoT
     }
 
     public static int countWords(String s){
@@ -9,20 +9,9 @@ public class Warmup {
         return words.length;
     }
 
-    public static int fizzBuzz(int input){
-        String fizz = "fizz";
-        String buzz = "buzz";
-        String fizzbuzz = "fizzbuzz";
-
-        for (int i = 0; i <= 100; i++){
-            if (input % 3 == 0){
-                System.out.println(fizz);
-            } else if (input % 5 == 0 ){
-                System.out.println(buzz);
-            } else {
-                System.out.println(fizzbuzz);
-            }
-        }
-        return 0;
+    public static String reverse(String str){
+        return new StringBuilder(str).reverse().toString();
     }
+
+
 }
